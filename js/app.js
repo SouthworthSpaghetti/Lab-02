ce47e41ca03e7e5f68327c43deca454da7e5e0d5
Anthony Horton <hortonadtwitter@gmail.com>
2020-01-07 20:22:01 -0800

    Added what I think is called a wrapper around userName prompts; so that if user doesn't input a name the code will run an alternate route.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var i = 1;
 var userName = prompt('What is your name?');
 var quizItemAnswerA = ['Anthony rides the ferry to work', 'YES', ''];
 var quizItemAnswerBb = ['hen hau translates to very bad, in Mandarin', 'NO', ''];
@@ -12,7 +13,14 @@ var quizItemAnswerD = ['The Highest Sea Cliffs in the world are in Alaska', 'NO'
 //var quizItemcontentBb = 'China';
 //var quizItemcontentC = 'Global Transportation';
 //var quizItemcontentD = 'Highest Sea Cliffs In The World';
-alert('Aloha, ' + userName + ', welcome to this website. Thank you for stopping by.');
+if (userName === ''){
+  do{
+    var userName = prompt('What is your name? What do your friends call you??');
+    i++;
+  } while(i<2);
+}else{
+  alert('Aloha, ' + userName + ', welcome to this website. Thank you for stopping by.');
+}
 quizItemAnswerA[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerA[0]);//CORRECT ANSWER IS: Y/YES
 quizItemAnswerBb[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerBb[0]);//CORRECT ANSWER IS: N/NO
 quizItemAnswerBa[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerBa[0]);//CORRECT ANSWER IS: Y/YES
@@ -40,5 +48,8 @@ console.log(quizItemAnswerD);
     alert("That doesn't make sense. But let's just move along.");
       break;
 }*/
-
-alert('Mahalo, ' + userName + ', thanks again for visiting. Have a great day.')
+if (userName === '') {
+  alert('Mahalo, you, thanks again for visiting. Have a great day.');
+  } else {
+  alert('Mahalo, ' + userName + ', thanks again for visiting. Have a great day.');
+};
