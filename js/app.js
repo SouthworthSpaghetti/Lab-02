01203fef9f592d3e87391f337a4482eb4bc7d17d
Anthony Horton <hortonadtwitter@gmail.com>
2020-01-07 16:33:12 -0800

    Finalized text content of page/CSS background image updated: no repeat/javaScript 4 quiz item values declared

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var userName;
+var quizItemcontentA = 'Ferry';
+var quizItemcontentB = 'China';
+var quizItemcontentC = 'Global Transportation';
+var quizItemcontentD = 'Highest Sea Cliffs In The World';
