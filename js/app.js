7769e6fddec86b74ee12213791c3088af4c48378
Anthony Horton <hortonadtwitter@gmail.com>
2020-01-07 21:42:40 -0800

    Left in comments some attempts to loops thru all 5 arrays. Am more succesful in bringing similar logic into new if else statements. Using chartAt instead of building new array index with y/n values.

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@
 
 var i = 1;
 var userName = prompt('What is your name?');
-var quizItemAnswerA = ['Anthony rides the ferry to work', 'YES', ''];
-var quizItemAnswerBb = ['hen hau translates to very bad, in Mandarin', 'NO', ''];
-var quizItemAnswerBa = ['Anthony studied abroad in China', 'YES', ''];
-var quizItemAnswerC = ['Anthony works in Global Transportation', 'YES', ''];
-var quizItemAnswerD = ['The Highest Sea Cliffs in the world are in Alaska', 'NO', ''];
+var quizItemAnswer1 = ['Anthony rides the ferry to work', 'YES', ''];
+var quizItemAnswer2 = ['hen hau translates to very bad, in Mandarin', 'NO', ''];
+var quizItemAnswer3 = ['Anthony studied abroad in China', 'YES', ''];
+var quizItemAnswer4 = ['Anthony works in Global Transportation', 'YES', ''];
+var quizItemAnswer5 = ['The Highest Sea Cliffs in the world are in Alaska', 'NO', ''];
 //ARRAY FOR ANSWERS?
 //var quizItemcontentA = 'Ferry';
 //var quizItemcontentBa = 'hen hau';
 //var quizItemcontentBb = 'China';
 //var quizItemcontentC = 'Global Transportation';
 //var quizItemcontentD = 'Highest Sea Cliffs In The World';
+//PERSONALIZED WELCOME MESSAGE
 if (userName === ''){
   do{
     var userName = prompt('What is your name? What do your friends call you??');
@@ -20,17 +21,33 @@ if (userName === ''){
   } while(i<2);
 }else{
   alert('Aloha, ' + userName + ', welcome to this website. Thank you for stopping by.');
+};
+quizItemAnswer1[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswer1[0]);//CORRECT ANSWER IS: Y/YES
+quizItemAnswer2[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswer2[0]);//CORRECT ANSWER IS: N/NO
+quizItemAnswer3[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswer3[0]);//CORRECT ANSWER IS: Y/YES
+quizItemAnswer4[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswer4[0]);//CORRECT ANSWER IS: Y/YES
+quizItemAnswer5[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswer5[0]);//CORRECT ANSWER IS: N/NO
+
+if (quizItemAnswer1[1].toUpperCase() === quizItemAnswer1[2].toUpperCase()) {
+  console.log('sucessA');
+  //alert('Good job, you answered correctly.');
+} else if (quizItemAnswer1[1].charAt(0).toUpperCase() === quizItemAnswer1[2].toUpperCase()){
+  console.log('sucessB');
+  //alert('Good job, you answered correctly.');
+} else{
+  console.log('bad');
+  //alert('That was close! But incorrect.');
 }
-quizItemAnswerA[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerA[0]);//CORRECT ANSWER IS: Y/YES
-quizItemAnswerBb[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerBb[0]);//CORRECT ANSWER IS: N/NO
-quizItemAnswerBa[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerBa[0]);//CORRECT ANSWER IS: Y/YES
-quizItemAnswerC[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerC[0]);//CORRECT ANSWER IS: Y/YES
-quizItemAnswerD[2] = prompt('Yes (Y)/No (N)--' + quizItemAnswerD[0]);//CORRECT ANSWER IS: N/NO
-console.log(quizItemAnswerA);
-console.log(quizItemAnswerBb);
-console.log(quizItemAnswerBa);
-console.log(quizItemAnswerC);
-console.log(quizItemAnswerD);
+
+
+
+console.log(quizItemAnswer1[1].charAt(0));
+console.log(quizItemAnswer1);
+console.log(quizItemAnswer2);
+console.log(quizItemAnswer3);
+console.log(quizItemAnswer4);
+console.log(quizItemAnswer5);
+
 /*switch(quizItemAnswerA.toUpperCase()){
   case 'Y':
     alert("That's correct.");
@@ -48,6 +65,16 @@ console.log(quizItemAnswerD);
     alert("That doesn't make sense. But let's just move along.");
       break;
 }*/
+//COMMENTED BELOW (AND CONSIDERING LOOPING THRU SWITCH ABOVE) I DO SOMETHING LIKE THIS IN VBA TO LOOP THRU MULTIPLE INSTANCES OF ARRAYS HERE. THE IDEA IS TO LOOP THRU QUIZ ANSWERS AND GENERATE MULTIPLE ALERTS TO USER ABOUT HOW THEY DID IN THEIR SCORE. ALL WITH MINIMAL CODE; where jk string value finds the arrays above.
+/*for (var j = 1; j < 6; j++){
+  var jk = 'quizItemAnswer' + j;
+  if (jk[1].toUpperCase === jk[2].toUpperCase){
+    alert('Good job, you answered correctly.');
+  }else
+    alert('That was close! But incorrect.');
+}*/
+
+//Greeting goodbye
 if (userName === '') {
   alert('Mahalo, you, thanks again for visiting. Have a great day.');
   } else {
